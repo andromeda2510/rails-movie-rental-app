3217714271bff6b85978f2d4d48f9c9bb9822b22
Andrea Miranda <amiranda2510@gmail.com>
2021-03-26 10:35:21 -0500

    fix bug in migration add user reference

--- db/migrate/20210325041939_add_user_ref_to_movies.rb
+++ db/migrate/20210325041939_add_user_ref_to_movies.rb
@@ -1,5 +1,5 @@
 class AddUserRefToMovies < ActiveRecord::Migration[6.1]
   def change
-    add_reference :movies, :user, foreign_key: true, null: true, default: null
+    add_reference :movies, :user, foreign_key: true, null: true
   end
 end
