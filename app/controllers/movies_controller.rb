d754b8ea60119e48cf252833ee7c4bf0d0f1f7c3
Andrea Miranda <amiranda2510@gmail.com>
2021-03-26 08:48:10 -0500

    add methods create update and destroy in controller and add link to these methods in card footer

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,15 +6,37 @@ class MoviesController < ApplicationController
     @movies = Movie.order(:name).all
   end
 
-  def new; end
+  def new
+    @movie = Movie.new
+  end
 
-  def create; end
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render 'new'
+    end
+  end
 
-  def edit; end
+  def edit
+    @movie = Movie.find(params[:id])
+  end
 
-  def update; end
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movies_path
+    else
+      render 'edit'
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
 
   def toggle_available_status
     @movie = Movie.find(params[:id])
@@ -41,7 +63,7 @@ class MoviesController < ApplicationController
   private
 
   def movie_params
-    params.require(:movie).permit(:name, :director, :release_date, :status)
+    params.require(:movie).permit(:name, :director, :release_date, :category_id, :status)
   end
 
   def user_params
