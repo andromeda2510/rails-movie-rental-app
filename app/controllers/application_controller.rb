69fbe7f894b28b1d307ddc391c2fcbe5ee268db8
andromeda2510 <amiranda2510@gmail.com>
2021-04-30 10:24:12 -0500

    fix rubocop offenses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
   before_action :update_allowed_parameters, if: :devise_controller?
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |user| 
-      user.permit(:name, 
-                  :email, 
-                  :password, 
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+      user.permit(:name,
+                  :email,
+                  :password,
                   :password_confirmation)
     end
   end
